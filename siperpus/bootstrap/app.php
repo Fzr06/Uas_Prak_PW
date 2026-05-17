e97a54b2acc49f96fb910eaab5ce62566ed2995f
febriansyah27 <81459533+febriansyah27@users.noreply.github.com>
2026-05-17 17:19:21 +0800

    menambahakan auth, provider ,midelware dan mengedit boostrap

--- siperpus/bootstrap/app.php
+++ siperpus/bootstrap/app.php
@@ -10,8 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'isAdmin'   => \App\Http\Middleware\IsAdmin::class,
+            'isAnggota' => \App\Http\Middleware\IsAnggota::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
